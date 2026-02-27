9118bf7a9f7f1c3fe13bf7fffb2074bcb18d4a9f
s.erkyn <sh.yerkin@kazpost.kz>
2026-02-27 15:54:50 +0500

    CYBERLY

---
+++ src/main/java/cb/empty/cyberly/CyberlyApplication.java
@@ -0,0 +1,13 @@
+package cb.empty.cyberly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cb/empty/cyberly/CyberlyApplicationTests.java
@@ -0,0 +1,13 @@
+package cb.empty.cyberly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
